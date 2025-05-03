d21fc57d313df16e5b992745cf6fd8ef7ee9f6df
Andres Suarez Guzman <andres_s18@outlook.com>
2025-05-03 00:06:43 -0500

    Cors added for frontend integration

--- src/Play.Identity.Service/Startup.cs
+++ src/Play.Identity.Service/Startup.cs
@@ -17,6 +17,7 @@ namespace Play.Identity.Service
 {
     public class Startup
     {
+        public const string AllowedOriginSetting = "AllowedOrigins";
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -72,6 +73,12 @@ namespace Play.Identity.Service
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Play.Identity.Service v1"));
+                app.UseCors(builder =>
+                {
+                    builder.WithOrigins(Configuration[AllowedOriginSetting])
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+                });
             }
 
             app.UseHttpsRedirection();

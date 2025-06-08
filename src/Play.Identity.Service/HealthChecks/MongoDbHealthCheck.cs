30d84198188407ba102f28f950e84d44064fb92d
Andres Suarez Guzman <andres_s18@outlook.com>
2025-06-08 13:19:26 -0500

    Add MongoDB health check implementation

---
+++ src/Play.Identity.Service/HealthChecks/MongoDbHealthCheck.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Duende.IdentityServer.Models;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using MongoDB.Driver;
+
+namespace Play.Identity.Service.HealthChecks;
+
+public class MongoDbHealthCheck : IHealthCheck
+{
+    private readonly MongoClient mongoClient;
+
+    public MongoDbHealthCheck(MongoClient mongoClient)
+    {
+        this.mongoClient = mongoClient;
+    }
+
+    public async Task<HealthCheckResult> CheckHealthAsync(
+        HealthCheckContext context,
+        CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            await mongoClient.ListDatabasesAsync(cancellationToken);
+            return HealthCheckResult.Healthy();
+        }
+        catch (Exception ex)
+        {
+            return HealthCheckResult.Unhealthy(exception: ex,
+                description: "MongoDB is not reachable or not responding.");
+        }
+    }
+}
